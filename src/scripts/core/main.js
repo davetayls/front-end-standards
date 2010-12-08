03cc1462b38b5c0889e8213690e6c2f150521b69
Dave Taylor <dave@the-taylors.org>
2010-12-08 15:29:06 +0000

    update site blueprint

--- src/scripts/core/main.js
+++ src/scripts/core/main.js
@@ -1,7 +1,5 @@
 require(["core/debug","h5f/h5f"], function() {
-    //This function is called when scripts/helper/util.js is loaded.
-
-    require.ready(function() {
+    $(function() {
         //This function is called when the page is loaded (the DOMContentLoaded
         //event) and when all required scripts are loaded.
 
